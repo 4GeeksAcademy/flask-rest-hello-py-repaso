4096e75b1e42dbc07b78a2c4c6dbcd884cd78cb1
Dayana Romero <68910675+dayanare@users.noreply.github.com>
2025-05-07 10:03:43 +0000

    endpoints

--- src/app.py
+++ src/app.py
@@ -9,14 +9,15 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
-#from models import Person
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,23 +28,59 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
-
     response_body = {
         "msg": "Hello, this is your GET /user response "
     }
-
     return jsonify(response_body), 200
+
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    if not data.get("email") or not data.get("password"):
+        return jsonify({"error", "email y pass obligatorio"}), 400
+
+    # consultar en la BD si el usuario existe en la BD
+    existing_user = User.query.filter_by(email=data["email"]).first()
+    if existing_user:
+        return jsonify({"error": "el email ya existe"}),  200
+
+    new_user = User(
+        email=data["email"],
+        password=data["password"],
+        is_active=True
+    )
+    db.session.add(new_user)
+    db.session.commit()
+
+    response_body = {
+        "msg": "Hello, this is your POST /user response ",
+        "data": new_user.serialize()
+    }
+    return jsonify(response_body), 200
+
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_user_id(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify({"error", "usuario no existe"}) , 404
+    return jsonify(user.serialize()), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -9,11 +9,24 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
-
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Pet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    age: Mapped[int] = mapped_column(nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+#tinder de mascota --> mascota(id, nombre, raza, edad, sexo),  propietario(nombre, telefono, ciudad)
+#tablas--> clases, 
